0229590170799d6f7e9466a573c9cb9a30a6f234
Snigdha Bose <sbose1@uncc.edu>
2018-11-22 17:22:23 -0500

    updated

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -109,20 +109,4 @@ exports.user_login = (req, res, next) => {
     });
 };
 
-exports.user_delete = (req, res, next) => {
-  User.remove({ _id: req.params.userId })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: "User deleted",
-        status: 200
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-        status: 500
-      });
-    });
-};
+
