cd00b68959c723d5fe93ca0468033dc263c77b0e
Snigdha Bose <sbose1@uncc.edu>
2018-11-22 16:33:57 -0500

    updated

---
+++ api/models/result.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const resultSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    evalId: {
+        type:String
+    },
+    teamId:{
+    	type:String
+    },
+    qId:{
+    	type:String
+    },
+    text:{
+        	type:String
+        },
+    surveyId:{
+        	type:String
+        },
+    answer:{
+            	type:Number
+            }
+});
+
+module.exports = mongoose.model('DeviceMapping', deviceSchema);
