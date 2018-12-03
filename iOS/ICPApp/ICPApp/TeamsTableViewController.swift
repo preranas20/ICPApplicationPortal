8dc6f017403c3ff00636649e367c6814f8ee0cf9
Ankit Kelkar <akelkar3@uncc.edu>
2018-12-03 01:18:47 -0500

    mobile login done

--- iOS/ICPApp/ICPApp/TeamsTableViewController.swift
+++ iOS/ICPApp/ICPApp/TeamsTableViewController.swift
@@ -1,52 +1,49 @@
 //
-//  LoginViewController.swift
+//  TeamsTableViewController.swift
 //  ICPApp
 //
-//  Created by Ankit Kelkar on 12/2/18.
+//  Created by Ankit Kelkar on 12/3/18.
 //  Copyright © 2018 Ankit Kelkar. All rights reserved.
 //
-
 import UIKit
 import QRCodeReader
 import AVFoundation
 import Alamofire
+import SwiftyJSON
 
-class LoginViewController: UIViewController ,QRCodeReaderViewControllerDelegate {
+class TeamsTableViewController: UITableViewController ,QRCodeReaderViewControllerDelegate {
     
-    @IBOutlet weak var password: UITextField!
-    @IBOutlet weak var email: UITextField!
+    var teams:NSArray = [];
     let RemoteIp:String = "http://52.202.147.130:5000/";
     override func viewDidLoad() {
-        
+         self.getTeams()
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func LoginPressed(_ sender: Any) {
-        let parameters: Parameters = [
-            "email": email.text!,
-            "password": password.text!,
-            "isPortal":false
-            
-        ]
-        Alamofire.request("\(RemoteIp)user/login", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+ func getTeams() {
+    let headers: HTTPHeaders = [
+        "Authorization": "Bearer \(self.readToken()) "
+    ]
+    Alamofire.request("\(RemoteIp)user/getTeam", headers:headers).responseJSON {  (response) in
             switch response.result {
             case .success:
                 
                 if let json = response.result.value {
-                   let JSON = json as! NSDictionary
-                   // print("JSON: \(JSON)")
-                    let data = JSON["data"] as! NSDictionary;
-                    let token = data["token"] as! String;
-                   self.saveToken(token: token)
-                print("Validation Successful")
-           self.performSegue(withIdentifier: "showTeams", sender: self)
+                    let j = json as! NSDictionary
+                    print("JSON: \(j)")
+                  self.teams = j["data"] as! NSArray
+                    print(self.teams.count)
+                    
+                    //self.saveToken(token: token)
+                    print("Validation Successful")
                 }
             case .failure(_):
                 print("some error occured")
             }
         }
-
+        
     }
     func readToken()-> String{
         let defaults = UserDefaults.standard
@@ -58,11 +55,10 @@ class LoginViewController: UIViewController ,QRCodeReaderViewControllerDelegate 
         let defaults = UserDefaults.standard
         defaults.set(token, forKey: "token")
     }
-    
     //QRCode specific
     
     // initialization and each time we need to scan a QRCode
-        var readerVC: QRCodeReaderViewController = {
+    var readerVC: QRCodeReaderViewController = {
         let builder = QRCodeReaderViewControllerBuilder {
             $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
         }
@@ -135,5 +131,75 @@ class LoginViewController: UIViewController ,QRCodeReaderViewControllerDelegate 
         present(readerVC, animated: true, completion: nil)
     }
     
-}
+    // MARK: - Table view data source
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return ((self.teams.count))
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "teamCell", for: indexPath)
+        let teamName = cell.viewWithTag(1) as! UITextView
+        let team = self.teams[indexPath.row] as! JSON
+       let name = team["teamName"]
+        print(name)
+        teamName.text = name.stringValue
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
