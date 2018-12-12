f40c06773756efa063c6b5de59e3c513ccf0dff6
Snigdha Bose <sbose1@uncc.edu>
2018-12-12 12:21:55 -0500

    updated function

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -10,7 +10,7 @@ module.exports.getTeam = function(req, res){
 
     User
       .find({})
-      .sort({score: -1})
+      ..sort('-score')
       .exec(function(err, user) {
         res.status(200).json({
           message:"Request successful",
